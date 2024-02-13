79090a544ad8402b09224f4d0cb50778bb4cab2d
Agniva Bhattacharya <agnivabhattacharya2001@gmail.com>
2024-02-13 21:25:36 +0530

    Text box added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
@@ -31,9 +31,8 @@ function App() {
   const API_KEY="AIzaSyB7qw1f805UDuVUVlyjz3gx0oRFB_OoQtU";
 
   const text="We choose to call ourselves as Usability Enthusiast (some people just refer us as “crazy”, but hey, who cares about “log-baag”). We are calling our this movement as “Only Usable”. We are still working on defining our online identity and legal entities. So, don't be surprised if you don’t find anything about us on net yet except this website. Remember this startup is currently in stealth mode and you are going to be part of its identity. So, lets join hands and build it together!!";
-
-  const [currentText,setCurrentText]=useState(text);
-
+  const textboxref=useRef(null);
+  const [currentText,setCurrentText]=useState("");
   const handleClick=(lang)=>{
     console.log(lang);
     
@@ -83,12 +82,9 @@ function App() {
 
   return (
     <div className="App">
-      {/* <button className="translate-button">Translate</button> */}
       <h1 className="heading">Translate Feature Assigment</h1>
       <div className="dropdown custom-dropdown">
         <DropdownButton id="dropdown-basic-button" title="Select Language">
-          
-          
           {allLangs.map(langdata=>{
             // console.log(langdata);
             return ( <Dropdown.Item onClick={()=>{
@@ -97,11 +93,26 @@ function App() {
           })}
         </DropdownButton>
     </div>
-      <div className="app-text">
+    <div className="app-text">
         <h3>
           {currentText}
         </h3>
       </div>
+
+      <button onClick={()=>{setCurrentText(text)}} className="translate-button">Use Sample Text</button>
+      <div  className="text-box">
+            <textarea ref={textboxref} className="text-box-area" name="multiLineInput" rows="10" cols="80" placeholder="Enter text here..."></textarea>
+            <button onClick={()=>{
+              console.log(textboxref.current.value);
+              setCurrentText(textboxref.current.value);
+              textboxref.current.value="";
+            }} className="translate-button add-text-button">Add Text</button>
+      </div>
+
+      <div className="ending-space"></div>
+
+
+      
     </div>
   );
 }
